7324e6d022e673138ba6da8050c93adebeb3cca1
minimao555 <75817142+minimao555@users.noreply.github.com>
2022-10-09 22:08:12 +0800

    可视化

--- data_extract.py
+++ data_extract.py
@@ -1,6 +1,11 @@
+from asyncore import read
 from io import TextIOWrapper
 import os
 import re
+import pandas as pd
+from matplotlib import pyplot as plt
+import matplotlib
+import csv
 from typing import Dict, List
 
 # global exec info static data
@@ -31,13 +36,41 @@ def update_global(single_dict: Dict[int, bool], passed: bool) -> None:
 			value_list[2 + offset] += 1
 		global_stat[lno] = value_list
 			
+def generator_csv():
+	df = pd.DataFrame.from_dict(global_stat,
+								orient='index',
+								columns=['N_cf', 'N_cs', 'N_uf', 'N_us'] )
+	df['N_f'] = df['N_cf'] + df['N_uf']
+	df['N_s'] = df['N_cs'] + df['N_us']
+	n = len(global_stat)
+	df['Tarantula'] = (df['N_cf']/df['N_f']) / (df['N_cf']/df['N_f'] + df['N_cs']/df['N_s'])
+	df['Zoltar'] = (df['N_cf'])/(df['N_cf'] + df['N_uf'] + df['N_cs'] + (10000 * df['N_uf'] * df['N_cs'])/(df['N_cf']))
+	df['Sokal'] = (2 * (df['N_cf'] + df['N_us']))/(2 * (df['N_cf'] + df['N_us']) + df['N_uf'] + df['N_cs'])
+	df['Fossum'] = (n * ((df['N_cf'] - 0.5) ** 2))/((df['N_cf'] + df['N_cs']) * (df['N_cf'] + df['N_uf']))
+	df.to_csv('output.csv')
 
-
+def draw_bar():
+	data = pd.read_csv('output.csv',usecols=['Tarantula','Zoltar','Sokal','Fossum'])
+	tarantula = list(data['Tarantula'])
+	zoltar = list(data['Zoltar'])
+	sokal = list(data['Sokal'])
+	fossum = list(data['Fossum'])
+	with open('./output.csv','r') as fp:
+		reader = csv.DictReader(fp)
+		index = [row[''] for row in reader]
+	
+	for list_name in ['tarantula','zoltar','sokal','fossum']:
+		fig = plt.figure(figsize=(12,6),dpi=100)
+		plt.bar(range(len(index[:50])),eval("{}[:50]".format(list_name)))
+		plt.xticks(range(len(index[:50])),index[:50])
+		plt.title(list_name)
+	plt.show()
 
 if __name__ == "__main__":
 	# extract execution info of lines
 	for fname in os.listdir("./gcov-res"):
 		with open("./gcov-res/{}".format(fname), "r") as f:
 			update_global(stat_exec(f), int(fname[:-5]) in passed_test)
-	
+	generator_csv()
+	draw_bar()
 	# TODO: Start From param calculate
